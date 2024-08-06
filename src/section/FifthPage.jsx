60225403751633f6a4ed8140ca014aeadaaf083a
Taran66 <taranpreet1911@gmail.com>
2024-08-06 11:13:25 +0530

    fifth page

--- src/section/FifthPage.jsx
+++ src/section/FifthPage.jsx
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Parser from 'rss-parser';
+import { BlogPosts } from "../constants"
 
 const FifthPage = () => {
-  const [blogPosts, setBlogPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchRssFeed = async () => {
-      const parser = new Parser();
-      const feed = await parser.parseURL('https://medium.com/feed/topic/technology');
-
-      setBlogPosts(
-        feed.items.map((item) => ({
-          date: item.pubDate,
-          heading: item.title,
-          content: item.contentSnippet,
-          src: item.enclosure?.url || '',
-        }))
-      );
-    };
-
-    fetchRssFeed();
-  }, []);
-
   return (
     <div className="max-container2 flex mt-40">
       <ul className="flex flex-col gap-10">
-        {blogPosts.map((item, index) => (
-          <li key={index} className="">
-            <a href="">
+          {BlogPosts.map((item) => (
+            <li key={item.label} className="">
+              <a href="">
               <div className="flex gap-10">
                 <div className="flex flex-col gap-2">
                   <p className="text-gray-400 text-lg">{item.date}</p>
@@ -36,13 +15,17 @@ const FifthPage = () => {
                 </div>
                 <img src={item.src} alt="blog-image" className="w-72" />
               </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div></div>
+              </a>
+              <img src="" alt="" />
+            </li>
+          ))}
+        </ul>
+        <div>
+            
+        </div>       
     </div>
-  );
-};
+    
+  )
+}
 
-export default FifthPage;
+export default FifthPage
